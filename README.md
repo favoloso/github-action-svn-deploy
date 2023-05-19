b3225a3af3e0dc0387cb3ac0d9cebd8c652e2cb2
Leonardo Ascione <leonardo.ascione@gmail.com>
2023-05-19 12:57:42 +0200

    Add missing $ command

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # github-action-svn-deploy
+
 GitHub Action per la pubblicazione e la sincronizzazione su SVN
+
+## Setup
+
+Realizzata con [`google/zx`](https://github.com/google/zx).
+
+## Utilizzo
+
+```yaml
+name: Deploy
+on:
+  push:
+    branches:
+      - master
+jobs:
+  commit:
+    name: Commit SVN
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v2
+      - name: Deploy
+        uses: favoloso/github-action-svn-deploy@main
+        with:
+          svn-user: ${{ secrets.SVN_USER }}
+          svn-password: ${{ secrets.SVN_PASSWORD }}
+          svn-url: ${{ secrets.SVN_URL }}
+          svn-path: ${{ secrets.SVN_PATH }}
+          dry-run: true # Opzionale, di default è false
+```
--- deploy.mjs
+++ deploy.mjs
@@ -1,22 +1,22 @@
 #!/usr/bin/env zx
 if (!process.env.INPUT_SVN_USERNAME) {
   echo`Missing INPUT_SVN_USERNAME`;
-  await `exit 1`;
+  await $`exit 1`;
 }
 
 if (!process.env.INPUT_SVN_PASSWORD) {
   echo`Missing INPUT_SVN_PASSWORD`;
-  await `exit 1`;
+  await $`exit 1`;
 }
 
 if (!process.env.INPUT_SVN_URL) {
   echo`Missing INPUT_SVN_URL`;
-  await `exit 1`;
+  await $`exit 1`;
 }
 
 if (!process.env.INPUT_SVN_PATH) {
   echo`Missing INPUT_SVN_PATH`;
-  await `exit 1`;
+  await $`exit 1`;
 }
 
 const svnDir = `${process.env.HOME}/svn-repo`;
@@ -27,22 +27,22 @@ const svnAuthFlags = [
 ]
 
 echo`➤ Checkout della repository SVN...`
-await `svn checkout "${process.env.INPUT_SVN_URL}" "${svnDir}" --depth immediates ${svnAuthFlags}`;
+await $`svn checkout "${process.env.INPUT_SVN_URL}" "${svnDir}" --depth immediates ${svnAuthFlags}`;
 cd(svnDir);
-await `svn update --set-depth infinity "${process.env.INPUT_SVN_PATH}" ${svnAuthFlags}`;
+await $`svn update --set-depth infinity "${process.env.INPUT_SVN_PATH}" ${svnAuthFlags}`;
 
 echo`➤ Copio i file...`
 if (!fs.existsSync(path.join(process.env.GITHUB_WORKSPACE, '.svnignore'))) {
   echo`ℹ︎ Impossibile trovare il file .svnignore, annullo il deploy.`;
-  await `exit 1`;
+  await $`exit 1`;
 }
-await `rsync -rc --exclude-from="${process.env.GITHUB_WORKSPACE}/.svnignore" "${process.env.GITHUB_WORKSPACE}/" "${process.env.INPUT_SVN_PATH}" --delete --delete-excluded`;
+await $`rsync -rc --exclude-from="${process.env.GITHUB_WORKSPACE}/.svnignore" "${process.env.GITHUB_WORKSPACE}/" "${process.env.INPUT_SVN_PATH}" --delete --delete-excluded`;
 
 echo`➤ Preparo i file per il commit...`
-await `svn add . --force`; // > /dev/null
-await `svn status "${process.env.INPUT_SVN_PATH}" | grep '^\!' | sed 's/! *//' | xargs -I% svn rm %@`; // > /dev/null
-await `svn update`;
-await `svn status`;
+await $`svn add . --force`; // > /dev/null
+await $`svn status "${process.env.INPUT_SVN_PATH}" | grep '^\!' | sed 's/! *//' | xargs -I% svn rm %@`; // > /dev/null
+await $`svn update`;
+await $`svn status`;
 
 const { version } = await fs.readJson(path.join(process.env.GITHUB_WORKSPACE, 'package.json'));
 
@@ -51,7 +51,7 @@ if (process.env.INPUT_DRY_RUN) {
 }
 else {
   echo`➤ Commit...`
-  await `svn commit -m "Aggiornamento automatico v${version}" --non-interactive ${svnAuthFlags}`;
+  await $`svn commit -m "Aggiornamento automatico v${version}" --non-interactive ${svnAuthFlags}`;
 }
 
 echo`✓ Rilasciato su SVN.`;
